b8c971a6bdf6a81b8dbebd07f011b71d4c0baa6b
RogerAirgood <rogerairgood@gmail.com>
2019-04-17 17:12:23 -0700

    Add files via upload

---
+++ post_install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Enable service
+
+sysrc -f /etc/rc.conf postgresql_enable="YES"
+
+# Initialize database
+service postgresql start
+sleep 5
+service postgresql initdb
+
+# Allow access from specified subnet
+
+# sed -i '' "s/127.0.0.1\/32/$SUBNET\/24/g" /var/db/postgres/data96/pg_hba.conf
+
+service postgresql restart
